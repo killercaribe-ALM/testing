5b3d15efee6ec32185ba7c56d72a230a720bb1ab
Killercaribe-ALM <rival@almus.dev>
2025-07-15 10:59:45 -0400

    Add packaging quantity field to product template and update views for better integration

--- almus_sale_product_packaging_qty/models/product_product.py
+++ almus_sale_product_packaging_qty/models/product_product.py
@@ -10,3 +10,16 @@ class ProductProduct(models.Model):
         help='Cantidad de unidades que contiene cada empaque del producto',
         tracking=True,
     )
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    
+    packaging_qty = fields.Integer(
+        string='Cantidad por Empaque',
+        default=1,
+        help='Cantidad de unidades que contiene cada empaque del producto',
+        tracking=True,
+        related='product_variant_ids.packaging_qty',
+        readonly=False,
+    )
